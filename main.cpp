1cb8805931213557fc0c596e72ca1ecc1fa27bb6
yuri.vilela@utec.edu.pe <PirataPrincipal2708>
2019-10-02 16:53:32 -0500

    third commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Gaaaaaa!!!!!" << std::endl;
+    double nota=0;
+    cin>>nota;
+
+    if(nota<10.5){
+        cout<<"Eres bica xd"<<endl;
+    }
+    else{
+        cout<<"Pasaste el curso GAAA!!!"<<endl;
+    }
     return 0;
 }
